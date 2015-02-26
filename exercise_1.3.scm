449b827e3c1d64abfb9d1bf132475c8a633a6f28
Raymond T. Hightower <rth@wisdomgroup.com>
2015-02-25 18:44:59 -0600

    count the fucking parentheses, raymond. or make your editor do it.

--- exercise_1.3.scm
+++ exercise_1.3.scm
@@ -2,17 +2,26 @@
 (define b 4)
 (define c 8)
 
-(define (sum_squares_of_two_larger a b c)
-  (cond (and (< a b) (< a c)) (+ (* b b) (* c c))
-        (and (< b a) (< b c)) (+ (* a a) (* c c))
-        (and (< c a) (< c b)) (+ (* a a) (* b b))
-        ))
-
 a
+(* a a)
+(+ (* b b) (* c c))
 
 b
+(* b b)
+(+ (* a a) (* c c))
 
 c
+(* c c)
+(+ (* a a) (* b b))
+
+(define (sum_squares_of_two_larger a b c)
+  (cond ((and (< a b) (< a c)) (+ (* b b) (* c c)))
+        ((and (< b a) (< b c)) (+ (* a a) (* c c)))
+        (else (+ (* a a) (* b b)))
+        ))
 
 (sum_squares_of_two_larger a b c)
+
+(sum_squares_of_two_larger 10 2 40)
+
 
