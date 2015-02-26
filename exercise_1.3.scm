4435e24242b9a713c9aaf2c6b525ca1e2a644770
Raymond T. Hightower <rth@wisdomgroup.com>
2015-02-25 18:09:08 -0600

    1.1 and 1.2 are complete

--- exercise_1.3.scm
+++ exercise_1.3.scm
@@ -1,5 +1,18 @@
+(define a 3)
+(define b 4)
+(define c 8)
+
 (define (sum_squares_of_two_larger a b c)
   (cond (and (< a b) (< a c)) (+ (* b b) (* c c))
-  (cond (and (< b a) (< b c)) (+ (* a a) (* c c))
-  (cond (and (< c a) (< c b)) (+ (* a a) (* b b))
-        ))))
+        (and (< b a) (< b c)) (+ (* a a) (* c c))
+        (and (< c a) (< c b)) (+ (* a a) (* b b))
+        ))
+
+a
+
+b
+
+c
+
+(sum_squares_of_two_larger a b c)
+
