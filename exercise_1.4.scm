859e6acb9b7ed188f726d0468ec8018d6e8faa51
Raymond T. Hightower <rth@wisdomgroup.com>
2015-02-25 18:54:41 -0600

    add workspaces for 1.4 and 1.5

--- exercise_1.4.scm
+++ exercise_1.4.scm
@@ -1,7 +1,7 @@
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
-; Expected behavior: Clever to add a plus the absolute value of b. 
+; Expected behavior: Clever way to add a plus the absolute value of b. 
 ; If b is greater than zero, then the procedure will return a + b.
 ; If b < 0, the procedure will return a - b. Either way the result will
 ; be a + |b|.
