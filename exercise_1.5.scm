a7418791ebb2fe9b0283bdd041d59f314fb38493
Raymond T. Hightower <rth@wisdomgroup.com>
2015-03-18 00:04:57 -0500

    thinking through 1.5

--- exercise_1.5.scm
+++ exercise_1.5.scm
@@ -2,6 +2,10 @@
 (define (test x y)
   (if (= x 0) 0 y))
 
+(test 0 (p))
 
+; What behavior will be observed with applicative-order evaluation?
+; What behavior will be observed with normal-order evaluation?
+; 
 
 
