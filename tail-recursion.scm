be5be325961da21181bdea6c77ac30319922d633
Raymond T. Hightower <rth@wisdomgroup.com>
2015-03-18 18:42:35 -0500

    add tail recursion example

---
+++ tail-recursion.scm
@@ -0,0 +1,14 @@
+(define (exp b n)
+  (exp-iter b n 1))
+
+(define (exp-iter b n product)
+  (if (eq? n 0)
+    product
+    (exp-iter b (- n 1) (* b product))))
+
+(exp 5 2)
+
+(exp 2 10)
+
+(exp 2 16)
+
