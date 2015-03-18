eb9c0b6286b14524a367e605fc77fcbd5aca9c2a
Raymond T. Hightower <rth@wisdomgroup.com>
2015-03-18 00:30:26 -0500

    more on exercise 1.6

--- exercise_1.6.scm
+++ exercise_1.6.scm
@@ -2,10 +2,28 @@
   (cond (predicate then-clause)
         (else else-clause)))
 
-; (define new-if 0)
-
+; Take new-if for a test run.
 (new-if (= 2 3) 0 5)
 
 (new-if (= 1 1) 0 5)
 
+; Create average function to support improve function.
+(define (average x y)
+  (/ (+ x y) 2))
+
+; Create improve function to support sqrt function.
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+; Try new-if with Newton's square root algorithm.
+(define (sqrt-iter guess x)
+  (new-if (good-enough? guess x)
+          guess
+          (sqrt-iter (improve guess x) x)))
+
+(sqrt-iter 5 10)
+
+(sqrt-iter 3 16)
+
+(sqrt-iter 20 999)
 
