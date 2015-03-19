aca109b359adc62a8063f3097d2e56c8ced05622
Raymond T. Hightower <rth@wisdomgroup.com>
2015-03-18 19:56:10 -0500

    create scratchpad for video 2

---
+++ video_2a.scm
@@ -0,0 +1,19 @@
+(define (sum-int a b)
+  (if (> a b)
+    0
+    (+ a
+       (sum-int (+ a 1) b))))
+
+(sum-int 2 3)
+
+(define (square num)
+  (* num num))
+
+(define (sum-sq a b)
+  (if (> a b)
+    0
+    (+ (square a)
+       (sum-sq (+ a 1) b))))
+
+(sum-sq 5 8)
+
