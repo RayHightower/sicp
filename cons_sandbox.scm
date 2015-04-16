2018dd62b1d049bcb20a52e3f7997b11383793f3
Raymond T. Hightower <rth@wisdomgroup.com>
2015-04-15 19:20:30 -0500

    scheme doesn not like the term nil. prefers the empty list, ()

--- cons_sandbox.scm
+++ cons_sandbox.scm
@@ -1,11 +1,18 @@
-(cons 1 
-      (cons 2 
-            (cons 3
-                  (cons 4 nil))))
-
+; nil is (), the empty list. Using 'nil generates an error.
+(define bar (cons 1 
+        (cons 2 
+              (cons 3
+                    (cons 4 ())))))
+  
+bar
 
 (list 1 2 3 4)
 
-(list 1-TO-4)
+; (list 1-TO-4)
+
+(define foo (list 1 2 3 4))
+
+(map square foo)
+
 
 
