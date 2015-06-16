318d44f525be860c393011f653963c3c2ea82140
Raymond T. Hightower <rth@wisdomgroup.com>
2015-06-16 11:29:13 -0500

    prep for next round of exercises

--- cons_sandbox.scm
+++ cons_sandbox.scm
@@ -3,16 +3,18 @@
         (cons 2 
               (cons 3
                     (cons 4 ())))))
-  
 bar
 
 (list 1 2 3 4)
 
-; (list 1-TO-4)
+; (list 1-to-4)
 
 (define foo (list 1 2 3 4))
 
 (map square foo)
 
+(reduce * 2 foo)
 
+(define foo ())
+(reduce * 2 foo)  ; if foo is nil, reduce returns the first value
 
